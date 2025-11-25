ed1ab168346520e184f39d4be35cb16329d29143
rohit1620 <malavrohit1620@gmail.com>
2025-11-25 10:21:18 +0530

    add something

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.style.display =
+    navLinks.style.display === "flex" ? "none" : "flex";
+});
